86294167078a21d1afaa301771e59964b8bc8ce1
danielsGitStuff <0nceupponatime>
2015-08-13 11:40:57 +0200

    fix: Directory did not manage its hashes correctly

--- src/data/Directory.java
+++ src/data/Directory.java
@@ -8,76 +8,76 @@ import io.FileSystem;
 import io.FSRelated;
 
 public class Directory extends FSRelated {
-	private HashSet<String> hashes = new HashSet<>();
-	private Directory parentDirectory;
-	private HashSet<Directory> subDirectories = new HashSet<>();
-	private HashSet<LeFile> leFiles = new HashSet<>();
-	private FileSystem fileSystem;
+    private Directory parentDirectory;
+    private HashSet<Directory> subDirectories = new HashSet<>();
+    private HashSet<LeFile> leFiles = new HashSet<>();
+    private FileSystem fileSystem;
 
-	public Directory(FileSystem fileSystem) {
-		this.fileSystem = fileSystem;
-	}
+    public Directory(FileSystem fileSystem) {
+        this.fileSystem = fileSystem;
+    }
 
-	public FileSystem getFileSystem() {
-		return fileSystem;
-	}
+    public FileSystem getFileSystem() {
+        return fileSystem;
+    }
 
-	public void addLeFile(LeFile leFile) {
-		hashes.add(leFile.getHash());
-		leFiles.add(leFile);
-	}
+    public void addLeFile(LeFile leFile) {
+        leFiles.add(leFile);
+    }
 
-	public void addSubDirectory(Directory subDir) {
-		subDirectories.add(subDir);
-	}
+    public void addSubDirectory(Directory subDir) {
+        subDirectories.add(subDir);
+    }
 
-	public void setParentDirectory(Directory parentDirectory) {
-		this.parentDirectory = parentDirectory;
-	}
+    public void setParentDirectory(Directory parentDirectory) {
+        this.parentDirectory = parentDirectory;
+    }
 
-	public Directory getParentDirectory() {
-		return parentDirectory;
-	}
+    public Directory getParentDirectory() {
+        return parentDirectory;
+    }
 
-	@Override
-	public String toString() {
-		return "[" + fileSystem.getPath() + "]";
-	}
+    @Override
+    public String toString() {
+        return "[" + fileSystem.getPath() + "]";
+    }
 
-	@Override
-	public void resetFlags() {
-		highlighted = false;
-		subDirectories.forEach(dir -> dir.resetFlags());
-		leFiles.forEach(f -> f.resetFlags());
-	}
+    @Override
+    public void resetFlags() {
+        highlighted = false;
+        subDirectories.forEach(dir -> dir.resetFlags());
+        leFiles.forEach(f -> f.resetFlags());
+    }
 
-	public HashSet<Directory> getSubDirectories() {
-		return subDirectories;
-	}
+    public HashSet<Directory> getSubDirectories() {
+        return subDirectories;
+    }
 
-	public HashSet<LeFile> getLeFiles() {
-		return leFiles;
-	}
+    public HashSet<LeFile> getLeFiles() {
+        return leFiles;
+    }
 
-	public HashSet<String> getHashes() {
-		return hashes;
-	}
+    public HashSet<String> getHashes() {
+        HashSet<String> result = new HashSet<>();
+        leFiles.stream().filter(f -> !f.isHidden()).forEach(leFile -> result.add(leFile.getHash()));
+        return result;
+    }
 
-	public void removeLeFile(LeFile leFile) {
-		leFiles.remove(leFile);
-		checkRemoval();
-	}
+    public void removeLeFile(LeFile leFile) {
+        leFiles.remove(leFile);
+        checkRemoval();
+    }
 
-	private void checkRemoval() {
-		if (subDirectories.size() == 0 && leFiles.size() == 0 && parentDirectory != null) {
-			parentDirectory.removeSubDirectory(this);
+    private void checkRemoval() {
+        if (subDirectories.size() == 0 && leFiles.size() == 0 && parentDirectory != null) {
+            parentDirectory.removeSubDirectory(this);
             hide();
         }
-	}
+    }
 
-	private void removeSubDirectory(Directory directory) {
-		subDirectories.remove(directory);
-		checkRemoval();
-	}
+    private void removeSubDirectory(Directory directory) {
+        subDirectories.remove(directory);
+        checkRemoval();
+    }
 
 }
